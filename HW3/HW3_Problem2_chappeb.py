890b6a08618634635089647d9f6e01a2bd59f971
ImDaNoob <bjchappell02@icloud.com>
2020-09-16 12:13:02 -0400

    problem 2 ready for testing

--- HW3/HW3_Problem2_chappeb.py
+++ HW3/HW3_Problem2_chappeb.py
@@ -2,7 +2,24 @@
 
 
 def main():
-    pass
+    # Stores the inputted fundemental frequency.
+    fundementalFreq = float(input("Enter the fundemental frequency for the string (in Hertz): "))
+    # Stores the lowest frequency of the band pass filter.
+    lowFreqFilter = float(input("Enter the lowest frequency of the band pass filter (in Hertz): "))
+    # Stores the highest frequency of the band pass filter.
+    highFreqFilter = float(input("Enter the highest frequency of the band pass filter (in Hertz): "))
+    # Stores the number of harmonics to investigate.
+    numHarmonics = float(input("How many harmonics do you want to investigate? "))
+
+    for harmonic in range(fundementalFreq, fundementalFreq * numHarmonics + 1, fundementalFreq):
+        curHarmonic = str(harmonic / fundementalFreq)
+        if harmonic > highFreqFilter or harmonic < lowFreqFilter:
+            print ("Harmonic " + curHarmonic + " is complete filtered")
+        elif harmonic < (1.1 * lowFreqFilter) or harmonic > (.9 * highFreqFilter):
+            print ("Harmonic " + curHarmonic + " is partially filtered")
+        else:
+            print ("Harmonic " + curHarmonic + " is not filtered")
+
 
 
 # Cautionary if, so that this file only executes main if it is meant to run main when executed or referenced.
