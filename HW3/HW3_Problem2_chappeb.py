d992e5411543930914d38631f80ce533ca32fc7d
ImDaNoob <bjchappell02@icloud.com>
2020-09-16 12:02:33 -0400

    starting problem 2

---
+++ HW3/HW3_Problem2_chappeb.py
@@ -0,0 +1,10 @@
+# Ben Chappell
+
+
+def main():
+    pass
+
+
+# Cautionary if, so that this file only executes main if it is meant to run main when executed or referenced.
+if __name__ == "__main__":
+    main()
