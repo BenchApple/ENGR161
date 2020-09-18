f185278310766683f3f1505dfae6821715dd19b4
ImDaNoob <bjchappell02@icloud.com>
2020-09-18 14:41:31 -0400

    finished problem 2

---
+++ PythonBasicsPractice/Py_Basics_ACT_Task2_chappeb.py
@@ -0,0 +1,16 @@
+# Ben Chappell
+
+def main():
+    temperature = 850 # hard coded value to store the tempurature of the system.
+    pressure = 17 # hard coded value to store the pressure of the system.
+
+    if temperature > 1000 or pressure > 20:
+        print("System is in Danger!")
+    elif temperature > 750 or pressure > 10:
+        print("System is normal.")
+    else:
+        print("System is in warning.")
+
+# Cationary if statement.
+if __name__ == "__main__":
+    main()
