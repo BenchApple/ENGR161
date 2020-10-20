b6e6e6f2d6f7b19b81ad81bf232b57347d4a89b1
Ben Chappell <bjchappell02@icloud.com>
2020-10-19 20:37:08 -0500

    sorting working

--- HW6/HW6_Prob1_chappeb.py
+++ HW6/HW6_Prob1_chappeb.py
@@ -51,45 +51,89 @@ def main():
     for plant in dataset:
         plant[2] = round(plant.pop(3) - plant[2], 2)
 
-    print (dataset)
-
-    # TODO Create some system to determine which parameter to sort by
     # Determine which method to sort by. An input P means Plant ID, S - Status
-    # G - growth data
+    # G - growth data, C - Concentration data.
+    # Personal code will be 0 - Plant ID, 1 - Status, 2 - Growth Data, 3 - Concentration. 
+    # This way it corresponds directly with the indices where those values should be stored in the dataset.
+    sortPara = sortingChoice(reoorgCode[0])
 
-    # TODO Create some method to sort by a specific parameter.
+    # Determine which direction to sort in. 0 means increasing, 1 means decreasing on my end. 
+    # From the user, A means increasing, D means decreasing.
+    sortDir = sortingDirection(reoorgCode[1])
 
+    # TODO Create some method to sort by a specific parameter. Custom comparison method most likely.
 
+    quicksort(dataset, sortPara, sortDir)
+    print(dataset)
 
 
     # Close our files.
     inputFile.close()
     outputFile.close()
     
+# Compares two lists, based on the input parameter and the direction of the sorting.
+# Returns true if n2 is greater than n1, false if the inverse when increasing, otherwise the opposite
+# p is the sorting parameter, dir is the direction (0 means increasing, 1 means decreasing)
+def comparePlants(p, dir, n1, n2):
+    if not dir:
+        return n1[p] < n2[p]
+    return n1[p] > n2[p]
 
-# Shell method for quicksort
-def quicksort(n):
-    qsort(n, 0, len(n) - 1)
+# Returns the personal sorting code depending on what the first letter of the code is.
+def sortingChoice(s):
+    c = -1
 
-# Quicksorts n from lo to hi. 
-def qsort(n, lo, hi):
+    if s == 'P':
+        c = 0
+    elif s == 'S':
+        c = 1
+    elif s == 'G':
+        c = 2
+    elif s == 'C':
+        c = 3
+
+    return c
+
+# Returns the sorting direction given the second letter of the code.
+def sortingDirection(s):
+    c = -1
+
+    if s == 'A':
+        c = 0
+    elif s == 'D':
+        c = 1
+
+    return c
+
+# Shell method for quicksort. p and dir determine the direction of the sorting.
+def quicksort(n, para, dir):
+    qsort(n, 0, len(n) - 1, para, dir)
+    
+
+# Quicksorts n from lo to hi. p and dir determine the parameter and direction of the sorting
+def qsort(n, lo, hi, para, dir):
     if lo < hi:
-        p = partition(n, lo, hi)
-        qsort(n, lo, p - 1)
-        qsort(n, p + 1, hi)
+        p = partition(n, lo, hi, para, dir)
+        qsort(n, lo, p - 1, para, dir)
+        qsort(n, p + 1, hi, para, dir)
 
 # Finds the partition for the quicksort. The partition is the method by which we effectively sort
 # The current section. It is found as the number of items in the list that are less than the 
 # value at n[hi]. By finding this value and swapping strategically with n[i] while iterating
 # through list n, we can sort the list recursively.
-def partition(n, lo, hi):
-    pivot = n[hi]
-    i = lo
+def partition(n, lo, hi, para, dir):
+    # If sorting in increasing order, set pivot to n[hi] and i to lo, otherwise the inverse.
+    if not dir:
+        pivot = n[hi]
+        i = lo
+    else:
+        pivot = n[lo]
+        i = hi
 
     # Iterate through all members of the current partition
     for j in range(lo, hi):
         # Swap the pivot with the current item if the pivot is greater than the current item.
-        if n[j] < pivot:
+        if comparePlants(para, dir, n[j], pivot):
             # Swap n[i] with n[j]
             temp = n[i]
             n[i] = n[j]
@@ -99,9 +143,15 @@ def partition(n, lo, hi):
             i += 1
 
     # Swap n[i] and n[hi]
-    temp = n[i]
-    n[i] = n[hi]
-    n[hi] = temp
+    if not dir:
+        temp = n[i]
+        n[i] = n[hi]
+        n[hi] = temp
+    # swap n[i] and n[lo] if sorting descending
+    else:
+        temp = n[i]
+        n[i] = n[lo]
+        n[lo] = temp
 
     # Return the value of i as the partition.
     return i
